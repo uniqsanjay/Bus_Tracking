25c4906a443c4617ffcf53957cdc37b22cea820e
uniqsanjay <uniq.sanjay@gmail.com>
2020-03-03 20:40:37 +0530

    Updated code

---
+++ app/src/main/java/com/mtcbustracking/bustracking/Driver.java
@@ -0,0 +1,16 @@
+package com.mtcbustracking.bustracking;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class Driver extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver);
+
+    }
+}
